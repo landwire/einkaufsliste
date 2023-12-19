9b875d9f24ff9e2238e9cc73ea50088152e5a07c
Sascha Paukner <sascha@landwire.net>
2023-12-19 15:14:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App () {
+    const [items, setItems] = useState([]);
+    const [itemName, setItemName] = useState('');
+    const [quantity, setQuantity] = useState(1);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const doneCount = items.filter((item) => item.done).length;
+    const totalCount = items.length;
+
+    // email
+    const emailSubject = 'Einkaufsliste';
+    const emailBody = encodeURIComponent(items.map((item) => item.name).join('\n'));
+    const mailtoLink = `mailto:?subject=${emailSubject}&body=${emailBody}`;
+
+    useEffect(() => {
+        // Load items from localStorage
+        const storedItems = JSON.parse(localStorage.getItem('shoppingListItems')) || [];
+        setItems(storedItems);
+    }, []);
+
+    useEffect(() => {
+        // Save items to localStorage whenever items change
+        localStorage.setItem('shoppingListItems', JSON.stringify(items));
+    }, [items]);
+
+    const handleInputChange = (e) => {
+        setItemName(e.target.value);
+        setErrorMessage('');
+    };
+
+    const handleInputKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleAddItem();
+        }
+    };
+
+    const handleQuantityChange = (e) => {
+        const value = parseInt(e.target.value, 10);
+        setQuantity(value);
+    };
+
+    const isFirstLetterUppercase = (str) => {
+        const firstChar = str.charAt(0);
+        return /^[A-Z]$/.test(firstChar);
+    };
+
+    const handleAddItem = () => {
+        if (itemName.trim() !== '' && isFirstLetterUppercase(itemName)) {
+            // create an array of newItems from input fields (quantity & text)
+            const newItems = Array.from({ length: quantity }, () => ({
+                name: itemName,
+                done: false
+            }));
+
+            // add newItems to existing items
+            setItems([...items, ...newItems]);
+
+            // reset values in input fields
+            setItemName('');
+            setQuantity(1);
+        } else {
+            setErrorMessage('Der Artikel muss mit einem großen Anfangsbuchstaben beginnen.');
+        }
+    };
+
+    // war nicht verlangt, macht aner denke ich Sinn, falls man sich mal vertan hat
+    const handleRemoveItem = (index) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+    };
+
+    const handleEmptyList = () => {
+        setItems([]);
+    };
+
+    const handleToggleDone = (index) => {
+        const updatedItems = [...items];
+        // Erlaube rückgängig machen, falls man aus Versehen einen Eintrag als "done" markiert hat
+        updatedItems[index].done = !updatedItems[index].done;
+        setItems(updatedItems);
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Einkaufsliste</h1>
+            </header>
+            <main>
+                <div className="inputArea">
+                    <div className="inputFields">
+                        <input
+                            className="itemQuantity"
+                            type="number"
+                            value={quantity}
+                            onChange={handleQuantityChange}
+                            min="1"
+                            max="20"
+                        />
+                        <input
+                            className="itemName"
+                            type="text"
+                            placeholder="Artikel"
+                            value={itemName}
+                            onChange={handleInputChange}
+                            onKeyDown={handleInputKeyDown}
+                        />
+                    </div>
+                    <button onClick={handleAddItem}>Add Item</button>
+                    {errorMessage && <p className="error-message">{errorMessage}</p>}
+                </div>
+                <div className="listArea">
+                    <ul>
+                        {items.map((item, index) => (
+                            <li
+                                key={index}
+                                className={item.done ? 'done' : ''}
+                                onClick={() => handleToggleDone(index)}
+                            >
+                                {item.name}
+                                <button onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleRemoveItem(index);
+                                }}><span className="visuallyHidden">Artikel entfernen</span>x
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    {totalCount > 0 && (
+                        <p className="itemsLeft">
+                            Noch {totalCount - doneCount} von {totalCount} Einträgen
+                        </p>
+                    )}
+                    {totalCount > 0 && (
+                        <button onClick={handleEmptyList}>Liste leeren</button>
+                    )}
+                    {totalCount > 0 && (
+                        <a className="linkAsButton" href={mailtoLink}>Einkaufsliste als E-Mail schicken</a>
+                    )}
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default App;
